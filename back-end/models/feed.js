8a4f24e496089665d91b52bda349dd0019215e3a
unknown <martin.j4n@gmail.com>
2020-06-06 22:37:11 +0200

    PUT and DELETE added to the back-end

--- back-end/models/feed.js
+++ back-end/models/feed.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("@hapi/joi");
 
 const feedSchema = new mongoose.Schema({
   content: {
@@ -11,4 +12,13 @@ const feedSchema = new mongoose.Schema({
 
 const Feed = mongoose.model("Feed", feedSchema);
 
+const validateFeed = (feed) => {
+  const schema = Joi.object({
+    content: Joi.string().min(3).max(255).required(),
+  });
+
+  return schema.validate(feed);
+};
+
 exports.Feed = Feed;
+exports.validate = validateFeed;
