48a3404a4e487073e4ed2ee7914cd6676e76d6e4
unknown <martin.j4n@gmail.com>
2020-06-08 15:41:29 +0200

    feed connected to the front-end

--- front-end/src/components/Feed/Feed.jsx
+++ front-end/src/components/Feed/Feed.jsx
@@ -4,61 +4,14 @@ import Cell from "../Cell/Cell";
 
 import styles from "./Feed.module.css";
 
-const Feed = () => {
-  let content = [
-    {
-      id: 1,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 2,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 3,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis? ",
-    },
-    {
-      id: 4,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 5,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 6,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 7,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 8,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-    {
-      id: 9,
-      content:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis?",
-    },
-  ];
-
+const Feed = ({ feed }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.innerWrapper}>
-        {content.map((el) => {
-          return <Cell key={el.id} content={el.content} />;
-        })}
+        {feed.length !== 0 &&
+          feed.map((el) => {
+            return <Cell key={el._id} content={el.content} />;
+          })}
       </div>
     </div>
   );
